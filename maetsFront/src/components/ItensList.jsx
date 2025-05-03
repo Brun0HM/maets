1e849d0e44d40c58253c2436dc228c749a524362
Brun0HM <brunodev41@gmail.com>
2025-05-03 15:04:38 -0300

    Carrinho descktop

--- maetsFront/src/components/ItensList.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ItensList = () => {
-  return (
-    <div>ItensList</div>
-  )
-}
-
-export default ItensList
--- maetsFront/src/pages/Cart.jsx
+++ maetsFront/src/pages/Cart.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import ItensList from "../components/ItensList";
 import Sumary from "../components/Sumary";
+import FavGames from "../components/FavGames";
+import cyberpunk from "../assets/Banner.png";
 
 const Cart = () => {
   return (
-    <div className="container d-flex">
-      <ItensList />
-      <Sumary jogo={"Cyberpunk 2077"} preco={79.95} />
+    <div className="row overflow-x-hidden">
+      <div className="container d-flex w-100 gap-5 align-items-center justify-content-center">
+        <div className="w-50">
+          <FavGames Banner={cyberpunk} titulo={"Cyberpunk"} genero={"RPG"} />
+        </div>
+        <Sumary jogo={"Cyberpunk 2077"} preco={79.95} />
+      </div>
     </div>
   );
 };
