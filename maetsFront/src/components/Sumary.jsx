6977a8b75becb877fcc83425fa4c845371b6de24
Brun0HM <brunodev41@gmail.com>
2025-05-03 14:42:50 -0300

    Colocando btn que eu esqueci

--- maetsFront/src/components/Sumary.jsx
+++ maetsFront/src/components/Sumary.jsx
@@ -8,12 +8,17 @@ const Sumary = (props) => {
         <div className="container d-flex flex-column align-items-center">
           <div className="d-flex justify-content-between gap-3 w-100">
             <h4 className="fs-5">{props.jogo}</h4>
-            <h4 className="fs-5">{props.preco}</h4>
+            <h4 className="fs-5">${props.preco}</h4>
           </div>
           <hr className="w-100" />
           <div className="d-flex justify-content-between w-100">
             <h4>Total</h4>
-            <h4 className="fs-5">{props.preco}</h4>
+            <h4 className="fs-5">${props.preco}</h4>
+          </div>
+          <div>
+            <h6 className="btn text-light btnCor ps-5 mt-3 pt-2 pb-2 pe-5">
+              Checkout
+            </h6>
           </div>
         </div>
       </div>
