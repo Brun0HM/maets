1388c38b06ee8cc6fc0c61799a657d38ce5347eb
Brun0HM <brunodev41@gmail.com>
2025-05-03 00:38:24 -0300

    comentando import logo

--- maetsFront/src/App.jsx
+++ maetsFront/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Profile from "./pages/Profile";
-import logo from "./assets/logo.png";
+// import logo from "./assets/logo.png";
 import Home from "./pages/Home";
 
 const App = () => {
