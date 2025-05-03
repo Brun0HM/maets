cb94e44458a72684a92733339c3c80fcb38feeec
Brun0HM <brunodev41@gmail.com>
2025-05-02 23:03:10 -0300

    Finalização do componente Destaque responsivo

--- maetsFront/src/pages/Home.jsx
+++ maetsFront/src/pages/Home.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Destaque from "../components/Destaque";
+import banner from "../assets/jojo.png";
+import Header from "../components/Header";
+import fotinha from "../assets/fotinha.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+      <Destaque
+        titulo={"JoJo's Bizarre Adventure: All-Star Battle R "}
+        descricao={
+          "JoJo's Bizarre Adventure: All-Star Battle R é um jogo de luta baseado no popular mangá e anime JoJo's Bizarre Adventure. O jogo apresenta uma variedade de personagens jogáveis."
+        }
+        preco={"R$ 199,99"}
+        Banner={banner}
+        fotinha={fotinha}
+      />
+    </div>
+  );
 };
 
 export default Home;
