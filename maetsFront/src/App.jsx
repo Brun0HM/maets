34ec7b00f2f7c129a133dbd073f114416a1be80f
Rattini <rattinicursods@gmail.com>
2025-05-06 08:32:41 -0300

    Header e Criação arquivo de tela de pagamento

--- maetsFront/src/App.jsx
+++ maetsFront/src/App.jsx
@@ -1,35 +1,19 @@
 import React from "react";
-import Profile from "./pages/Profile";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Header from "./components/Header";
 import logo from "./assets/logo.png";
 import profile from "./assets/Profile.png";
-=======
-// import logo from "./assets/logo.png";
-import Home from "./pages/Home";
->>>>>>> main
-=======
-import logo from "./assets/logo.png";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
->>>>>>> main
+import Payment from "./pages/Payment";
 
 const App = () => {
   return (
     <div>
-<<<<<<< HEAD
-<<<<<<< HEAD
+
       <Header Logo={logo} Profile={profile}/>
-=======
-      {/* <Profile Logo={logo} /> */}
-      <Home />
->>>>>>> main
-=======
       {/* <Profile Logo={logo} />
       <Home /> */}
-      <Cart />
->>>>>>> main
+      <Payment />
     </div>
   );
 };
