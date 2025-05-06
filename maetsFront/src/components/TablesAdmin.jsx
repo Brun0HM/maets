437c281c957691c37d540bf714383053465535a6
Brun0 <brunodev41@gmail.com>
2025-05-06 14:11:27 -0300

    btn adicionar jogo

--- maetsFront/src/components/TablesAdmin.jsx
+++ maetsFront/src/components/TablesAdmin.jsx
@@ -4,14 +4,17 @@ const TablesAdmin = () => {
   return (
     <div className="container conteudo rounded-3 d-flex flex-column align-items-center justify-content-center mt-5 pb-3">
       <div className="input-group mt-2 d-flex justify-content-center">
+        <a href="#">
+          <i class="bi bi-plus-square btn btn-outline-success rounded-2 me-2"></i>
+        </a>
         <input
-          className="border-0 rounded-2 conteudo text-light align-items-center justify-content-center rounded-end-0"
+          className="border-0  border-bottom  conteudo text-light align-items-center justify-content-center rounded-end-0"
           placeholder="Search..."
           type="text"
         />
-        <i className="bi bi-search btn conteudo text-light text-end"></i>
+        <i className="bi bi-search btn conteudo text-light text-end border-bottom "></i>
       </div>
-      <div className="d-flex flex-column w-50 mt-2 favGamesFundo p-2 rounded-2 overflow-x-auto">
+      <div className="d-flex flex-column w-50 mt-2 favGamesFundo p-2 rounded-2 overflow-y-auto overflow-x-hidden">
         {/* Linha 1 */}
         <div className="row mb-2">
           <div className="col-6 text-start">
