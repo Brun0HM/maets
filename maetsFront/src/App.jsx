212570251d786068c5d778c7b1b4289b162d5410
Thiago Mazzi <thiagodev728@gmail.com>
2025-05-06 07:53:30 -0300

    Apagado carrinho

--- maetsFront/src/App.jsx
+++ maetsFront/src/App.jsx
@@ -3,12 +3,10 @@ import Profile from "./pages/Profile";
 import Login from "./pages/Login";
 import logo from "./assets/Logo.png";
 import Modal from "./components/Modal";
-import Cartinhos from "./components/Cartinhos";
 
 const App = () => {
   return (
     <div>
-     <Cartinhos />
     </div>
   );
 };
