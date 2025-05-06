70ae16619f176cb87a558f0c86fd651446ec12af
Thiago Mazzi <thiagodev728@gmail.com>
2025-05-06 09:04:02 -0300

    app

--- maetsFront/src/App.jsx
+++ maetsFront/src/App.jsx
@@ -3,9 +3,7 @@ import Header from "./components/Header";
 import logo from "./assets/logo.png";
 import profile from "./assets/Profile.png";
 import "./App.css";
-import Profile from "./pages/Profile";
 import Login from "./pages/Login";
-import logo from "./assets/Logo.png";
 import Modal from "./components/Modal";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
@@ -19,9 +17,7 @@ const App = () => {
       <Header Logo={logo} Profile={profile}/>
       {/* <Profile Logo={logo} />
       <Home /> */}
-      <Payment />
       < Footer Logo={logo} />
-      <Modal/>
     </div>
   );
 };
