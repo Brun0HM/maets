a26231480b3b8fd36364e000cc4a20c382d5bfa3
Rattini <rattinicursods@gmail.com>
2025-05-06 16:54:23 -0300

    payment

--- maetsFront/src/pages/Payment.jsx
+++ maetsFront/src/pages/Payment.jsx
@@ -4,7 +4,6 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import logo from "../assets/Logo.png";
 import perfito from "../assets/profile.png";
-import React from 'react'
 import PurchaseSummary from '../components/PurchaseSummary'
 import RegisterCard from '../components/RegisterCard'
 import RegisteredCards from '../components/RegisteredCards'
@@ -13,18 +12,15 @@ const Payment = () => {
   return (
     <>
       <Header Logo={logo} Profile={perfito}/>
-    <PayCard />
-    <Footer Logo={logo}/>
-      <RegisterCard />
-      <PurchaseSummary />
-    </>
-    <div className="container my-5">
+      <div className="container my-5">
       <div className="d-flex justify-content-center gap-4 flex-wrap">
         <RegisteredCards />
         <RegisterCard />
         <PurchaseSummary />
       </div>
     </div>
+    <Footer Logo={logo}/>
+    </>
   )
 }
 
