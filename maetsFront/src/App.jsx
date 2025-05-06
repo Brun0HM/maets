6a7d5b657df2067264eea6c2fb26b6c091509225
Rattini <rattinicursods@gmail.com>
2025-05-06 14:49:42 -0300

    .

--- maetsFront/src/App.jsx
+++ maetsFront/src/App.jsx
@@ -6,10 +6,21 @@ import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Payment from "./pages/Payment";
 import Footer from "./components/Footer";
+<<<<<<< Updated upstream
+=======
+import Profile from "./pages/Profile";
+import DashBoard from "./pages/DashBoard";
+import TablesAdmin from "./components/TablesAdmin";
+import FormCupom from "./components/FormCupom";
+import FormGames from "./components/FormGames";
+import FormCate from "./components/FormCate";
+import PayCard from "./components/PayCard";
+>>>>>>> Stashed changes
 
 const App = () => {
   return (
     <div>
+      <PayCard />
     </div>
   );
 };
