b63b9938eb6eb6703222ee981aff907a26a636a0
Rattini <rattinicursods@gmail.com>
2025-05-06 14:55:54 -0300

    ..

--- maetsFront/src/App.jsx
+++ maetsFront/src/App.jsx
@@ -1,26 +1,17 @@
 import React from "react";
 import Header from "./components/Header";
-import logo from "./assets/logo.png";
-import profile from "./assets/Profile.png";
+import Logo from "./assets/logo.png";
+import Profile from "./assets/Profile.png";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Payment from "./pages/Payment";
 import Footer from "./components/Footer";
-<<<<<<< Updated upstream
-=======
-import Profile from "./pages/Profile";
-import DashBoard from "./pages/DashBoard";
-import TablesAdmin from "./components/TablesAdmin";
-import FormCupom from "./components/FormCupom";
-import FormGames from "./components/FormGames";
-import FormCate from "./components/FormCate";
 import PayCard from "./components/PayCard";
->>>>>>> Stashed changes
 
 const App = () => {
   return (
     <div>
-      <PayCard />
+      <Payment />
     </div>
   );
 };
