8acd3fa3691c0931821325635611fe7a51407e29
Rattini <rattinicursods@gmail.com>
2025-05-07 10:13:45 -0300

    Editar Perfil v1

--- maetsFront/src/components/ChangeProfilePic.jsx
+++ maetsFront/src/components/ChangeProfilePic.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 
 const ChangeProfilePic = (props) => {
   return (
-    <div>
-        <img className="rounded-3 m-3" src={props.Profile} alt="" />
-        
+    <div className=' d-flex flex-column align-items-center'>
+
+      <div>
+        <img 
+          className="rounded-3 rounded-bottom-0 img-fluid" 
+          width="129.60px"
+          src={props.foto} 
+          alt="Profile"
+        />
+      </div>
+
+      <div className='w-100 text-center'>
+        <button className="btnCor text-light border-0 py-2 px-4 rounded-2 rounded-top-0 w-100 w-md-auto">
+          Edit Profile
+        </button>
+      </div>
+
     </div>
   )
 }
