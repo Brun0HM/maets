e00163c737f223ec1f969f7d48a9ad2e76dcf784
Brun0 <brunodev41@gmail.com>
2025-05-13 07:43:46 -0300

    Refactor Header component: clean up code and improve formatting

--- maetsFront/src/components/Header.jsx
+++ maetsFront/src/components/Header.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import "../app.css";
 import { Link } from "react-router";
 
 const Header = (props) => {
-    const [username, setUsername] = useState('');
-    const [foto, setFoto] = useState('');
-  
-    useEffect(() => {
-      // Recupera os dados do localStorage quando o componente for montado
-      const storedData = JSON.parse(localStorage.getItem('devlogin'));
-    
-      if (storedData) {
-        if (storedData.username) {
-          setUsername(storedData.username);
-        }
-        if (storedData.foto) {
-          setFoto(storedData.foto);
-        }
+  const [username, setUsername] = useState("");
+  const [foto, setFoto] = useState("");
+
+  useEffect(() => {
+    // Recupera os dados do localStorage quando o componente for montado
+    const storedData = JSON.parse(localStorage.getItem("devlogin"));
+
+    if (storedData) {
+      if (storedData.username) {
+        setUsername(storedData.username);
       }
-    }, []);
+      if (storedData.foto) {
+        setFoto(storedData.foto);
+      }
+    }
+  }, []);
 
   return (
     <nav className="navbar navbar-expand-lg px-4">
@@ -64,7 +63,10 @@ const Header = (props) => {
             <div className="d-flex align-items-center justify-content-between gap-2">
               {/* Nome e botão de perfil */}
               <div className="text-end">
-                <h1 className="text-light fw-bold mb-0 fs-5"> Welcome, {username || 'Guest'}! </h1>
+                <h1 className="text-light fw-bold mb-0 fs-5">
+                  {" "}
+                  Welcome, {username || "Guest"}!{" "}
+                </h1>
                 <Link to="/profile" className="text-decoration-none text-light">
                   <i className="bi bi-chevron-down me-1 text-light"></i> See
                   Profile
